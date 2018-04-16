454c7027f58dc959676919a68edcdeab8b2736c5
Nikul <nikul@ee.columbia.edu>
2018-04-16 16:30:00 -0400

    Update Docker run script

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,2 +1,8 @@
-docker rm neuroarch_component
-docker run --name neuroarch_component -v $(dirname `pwd`):/neuroarch_component -v $(dirname $(dirname `pwd`))/neuroarch:/neuroarch -it ffbo/neuroarch_component:develop sh /neuroarch_component/neuroarch_component/run_component_docker.sh
+DOCKER_NAME = "ffbo.neuroarch_component"
+if [ "$#" -eq 1 ]
+then
+    DOCKER_NAME = $1
+fi
+
+docker rm $DOCKER_NAME
+docker run --name $DOCKER_NAME -v $(dirname `pwd`):/neuroarch_component -v $(dirname $(dirname `pwd`))/neuroarch:/neuroarch -it ffbo/neuroarch_component:develop sh /neuroarch_component/neuroarch_component/run_component_docker.sh
